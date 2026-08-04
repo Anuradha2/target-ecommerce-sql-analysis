c2033880eca0b0f1b63638d1186cc3b5417760b7
Anuradha Sarkar <39532683+Anuradha2@users.noreply.github.com>
2026-08-04 23:57:24 +0530

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,15 @@
+/*======================================================================
+Project : Target Brazil E-commerce Analysis
+Author  : Anuradha Sarkar
+Tool    : Google BigQuery
+Language: SQL
+
+Description:
+This project analyzes the Target Brazil E-commerce dataset to understand
+customer purchasing behavior, sales trends, logistics performance,
+freight costs, and payment patterns.
+
+======================================================================*/
+
+
+
